2b9b27fa3f00b33457e543c91dd6ce4e6263f56a
kai8582 <juho8582@gmail.com>
2023-05-28 15:51:48 +0900

    settings.py에 템플렛 디렉토리 설정

--- HotPlaceList/HotPlaceList/settings.py
+++ HotPlaceList/HotPlaceList/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'HotPlaceList.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
