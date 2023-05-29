a4a1cab95f98e2f7d45d87d015f2da87750d768d
kai8582 <juho8582@gmail.com>
2023-05-29 17:43:04 +0900

    빠진 views 올림

--- HotPlaceList/hotplist/views.py
+++ HotPlaceList/hotplist/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import HPForm,ReviewForm
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
     return render(request, 'hotplist/index.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        form = HPForm(request.POST, request.FILE)
+        if form.is_valid():
+            form.save()
+            return redirect(request, 'hotplist:index')
+        else:
+            messages.error(request, "작성 양식에 맞지않습니다. 다시 작성해주세요")
+            context = {'form': form}
+            return render(request, 'hotplist/create.html',context)
+    else:
+        form = HPForm()
+        return render(request, 'hotplist/create.html',{'form':form})
+
+
