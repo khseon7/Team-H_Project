69ffbaea9407d0c871caec4205af2d48ed4d03bc
khseon7 <gkrtjs5544@gmail.com>
2023-05-29 22:32:24 +0900

    models.py 생성

--- HotPlaceList/hotplist/models.py
+++ HotPlaceList/hotplist/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class HotPlace(models.Model):
+    placename=models.CharField(max_length=50)
+    address=models.CharField(max_length=100)
+    callnum=models.CharField(max_length=20)
+    eval=models.DecimalField(max_digits=5,decimal_places=1)
+    image=models.ImageField(upload_to='images/')
+    
+class WantList(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE,default="")
+    hotplace=models.ForeignKey(HotPlace,on_delete=models.CASCADE,default="")
+
+class Review(models.Model):
+    author=models.ForeignKey(User,on_delete=models.CASCADE,default="")
+    content=models.CharField()
+    hotplace=models.ForeignKey(HotPlace,on_delete=models.CASCADE,)
+    eval=models.DecimalField(max_digits=5,decimal_places=1)
+    date=models.DateTimeField()
+    
--- HotPlaceList/hotplist/views.py
+++ HotPlaceList/hotplist/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'hotplist/index.html')
+
+def detail(request):
+    hotplace=hotplace.objects.all()
