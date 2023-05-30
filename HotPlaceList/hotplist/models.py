32681cb1a10db865e3061e0c602bb629a6f10bad
khseon7 <gkrtjs5544@gmail.com>
2023-05-30 23:55:38 +0900

    models 수정, forms 생성 중

--- HotPlaceList/hotplist/models.py
+++ HotPlaceList/hotplist/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ValidationError
 
+def validate_rating(value):
+    if value >5.0 | value<0.0:
+        raise ValidationError(
+            ('0.0에서 5.0 사이 값을 입력해주세요')
+        )
 
 # Create your models here.
-class HotPlace(models.Model):
-    placename=models.CharField(max_length=50)
-    address=models.CharField(max_length=100)
-    callnum=models.CharField(max_length=20)
-    eval=models.DecimalField(max_digits=5,decimal_places=1)
+class HotPlaces(models.Model):
+    placename=models.CharField(max_length=100,null=False)
+    address=models.CharField(max_length=120,null=False)
+    phone=models.CharField(max_length=15,null=True,blank=True)
+    rating=models.DecimalField(max_digits=1,decimal_places=1,validators=[validate_rating],default=0.0)
     image=models.ImageField(upload_to='images/')
     
 class WantList(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE,default="")
-    hotplace=models.ForeignKey(HotPlace,on_delete=models.CASCADE,default="")
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    hotplace=models.ForeignKey(HotPlaces,on_delete=models.CASCADE)
 
 class Review(models.Model):
-    author=models.ForeignKey(User,on_delete=models.CASCADE,default="")
-    content=models.CharField()
-    hotplace=models.ForeignKey(HotPlace,on_delete=models.CASCADE,)
-    eval=models.DecimalField(max_digits=5,decimal_places=1)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.CharField(max_length=200,null=True)
+    place=models.ForeignKey(HotPlaces,on_delete=models.CASCADE)
+    rating=models.DecimalField(max_digits=1,decimal_places=1,validators=[validate_rating])
     date=models.DateTimeField()
-    
--- HotPlaceList/hotplist/urls.py
+++ HotPlaceList/hotplist/urls.py
@@ -7,4 +7,5 @@ app_name = "hotplist"
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('detail/',views.detail,name="detail"),
+    path('signup/',views.signup,name="signup"),
 ]
