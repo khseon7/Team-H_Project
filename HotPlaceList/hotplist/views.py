5cd83a3f93c0c58f6de808c40e173d2f3384d249
khseon7 <gkrtjs5544@gmail.com>
2023-06-01 00:04:34 +0900

    회원가입 오류 해결

--- HotPlaceList/hotplist/views.py
+++ HotPlaceList/hotplist/views.py
@@ -3,11 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import UserForm
 from .models import HotPlaces,Review
-from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
-
-
 
 # Create your views here.
 def index(request):
@@ -20,12 +17,15 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            login(request, user)  # 로그인
-            return redirect('index')
+            user = authenticate(request, username=username, password=raw_password)  # 사용자 인증
+            auth_login(request, user)  # 로그인
+            return redirect('hotplist:index')
+        
+        else:
+            return redirect('hotplist:signup')
     else:
         form = UserForm()
-    return render(request, 'hotplist/signup.html', {'form': form})
+        return render(request, 'hotplist/signup.html', {'form': form})
 
 def login(request):
     if request.method == "POST":
@@ -36,7 +36,6 @@ def login(request):
             auth_login(request, user)
             return redirect('hotplist:detail')
         else:
-            print("Not User")
             return redirect('hotplist:login')
         
     else:
